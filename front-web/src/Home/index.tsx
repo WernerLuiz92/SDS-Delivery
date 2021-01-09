a6e605e0101e3930fa4c50fc7afbd4f3fe553fb2
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-01-09 01:13:19 -0300

    Created Home component

---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function Home() {
+    return (
+        <div className="home-container">
+            <div className="home-content">
+                <h1>Home</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
