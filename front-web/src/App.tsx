824996ded507fcb7963e735eff388dc99494a084
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-01-08 23:34:50 -0300

    Simple Counter Example

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 
-import Hello from './Hello';
+import Counter from './Counter';
+
 
 function App() {
   return (
     <div>
-      <Hello message="World"/>
+      <Counter />
     </div>
   );
 }
