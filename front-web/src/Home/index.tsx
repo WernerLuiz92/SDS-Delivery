85b59e8874ac4d5ba5cdf9c811f4d62aafe47e59
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-01-10 00:26:04 -0300

    Add HomePage

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,10 +1,26 @@
 import './styles.css';
 
+import { ReactComponent as MainImage } from './main.svg';
+
 function Home() {
     return (
         <div className="home-container">
             <div className="home-content">
-                <h1>Home</h1>
+                <div className="home-actions">
+                    <h1 className="home-title">
+                        Faça seu pedido <br/>
+                        que entregamos <br/>
+                        para você!!
+                    </h1>
+                    <h3 className="home-subtitle">
+                        Escolha o seu pedido e em poucos minutos <br/>
+                        levaremos na porta de sua casa!
+                    </h3>
+                    <a href="order" className="home-btn-order" >FAZER PEDIDO</a>
+                </div>
+                <div className="home-image">
+                    <MainImage />
+                </div>
             </div>
         </div>
     )
